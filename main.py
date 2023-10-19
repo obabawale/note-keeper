4ccebf5c2f54618be5acded4f1a300f82c3d4463
obabawale <padinality@yahoo.com>
2023-10-19 08:54:56 +0100

    Added entrypoint for the application

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-import uvicorn
+from typing import Union
+
 from fastapi import FastAPI
+
 app = FastAPI()
 
-@app.post("/user/", response_model=User)
-def create_user(user: User):
-    return user
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
